f9192df86920c7c0cf92133b3f6b1ea40cc5ed5e
Vladislav Kostylev <kostylev.v@gmail.com>
2024-05-21 17:03:50 +0300

    feat: films

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,11 +2,19 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class FilmorateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
+		Film film = new Film();
+		film.setDescription("Desc");
+		film.setName("Name");
+		film.setReleaseDate(LocalDate.now());
+		film.setDuration(60);
 	}
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,30 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.controller.ReleaseDateConstraint;
+
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+    private long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @Size(max = 200, message
+            = "Description must be between 0 and 200 characters")
+    private String description;
+
+    @ReleaseDateConstraint
+    private LocalDate releaseDate;
+
+    @Positive
+    private int duration;
 }

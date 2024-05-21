514cbdb6b6973beaf6bfef62fb8b91f44a963314
Vladislav Kostylev <kostylev.v@gmail.com>
2024-05-21 17:03:50 +0300

    feat: films

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -1,12 +1,33 @@
 package ru.yandex.practicum.filmorate;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.yandex.practicum.filmorate.model.DurationAdapter;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.LocalDateTypeAdapter;
+
+import java.time.Duration;
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class FilmorateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
+
+		Film film = new Film();
+		film.setDescription("Desc");
+		film.setName("Name");
+		film.setReleaseDate(LocalDate.now());
+		film.setDuration(Duration.ofMinutes(25));
+
+		GsonBuilder builder = new GsonBuilder();
+		builder.registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter());
+		builder.registerTypeAdapter(Duration.class, new DurationAdapter());
+		Gson gson = builder.create();
+		System.out.println(gson.toJson(film));
+
 	}
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,84 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Controller for films
+ */
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private final Map<Long, Film> films = new HashMap<>();
+    private static final LocalDate MIN_RELEASE_DATE = LocalDate.of(1895, 12, 28);
+    private static final int MAX_DESC_LENGTH = 200;
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@RequestBody Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            log.error("Title shouldn't be empty");
+            throw new ValidationException("название не может быть пустым");
+        }
+
+        if (film.getDescription() != null && film.getDescription().length() > MAX_DESC_LENGTH) {
+            log.error("Max description length is {} , got {} ", MAX_DESC_LENGTH, film.getDescription().length());
+            throw new ValidationException("максимальная длина описания - " + MAX_DESC_LENGTH + " символов");
+        }
+
+        if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(MIN_RELEASE_DATE)) {
+            log.error("Release date couldn't be before {}, got {}", MIN_RELEASE_DATE, film.getReleaseDate());
+            throw new ValidationException("дата релиза — не раньше " + MIN_RELEASE_DATE);
+        }
+
+        if (film.getDuration() == null || film.getDuration().isZero() || film.getDuration().isNegative()) {
+            log.error("Film duration should be positive, got {}", film.getDuration());
+            throw new ValidationException("продолжительность фильма должна быть положительным числом");
+        }
+
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.trace("Added film {}", film);
+        return film;
+    }
+
+    /*
+    @PutMapping
+    public Post update(@RequestBody Post newPost) {
+        if (newPost.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        if (posts.containsKey(newPost.getId())) {
+            Post oldPost = posts.get(newPost.getId());
+            if (newPost.getDescription() == null || newPost.getDescription().isBlank()) {
+                throw new ConditionsNotMetException("Описание не может быть пустым");
+            }
+            oldPost.setDescription(newPost.getDescription());
+            return oldPost;
+        }
+        throw new NotFoundException("Пост с id = " + newPost.getId() + " не найден");
+    }
+*/
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }

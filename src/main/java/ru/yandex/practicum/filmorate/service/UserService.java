4b9a234d4112a8eac2dc2a088c121bb25b188c28
Vladislav Kostylev <kostylev.v@gmail.com>
2024-06-14 12:26:09 +0300

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -8,7 +8,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 

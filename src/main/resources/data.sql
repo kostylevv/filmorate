8174700dd7b2329acb09af4954d7f51ebcb511f4
Vladislav Kostylev <kostylev.v@gmail.com>
2024-07-01 17:04:53 +0300

    feat: h2 setup and schema

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO genre (title) VALUES ('Comedy');
+INSERT INTO genre (title) VALUES ('Drama');
+INSERT INTO genre (title) VALUES ('Horror');
+INSERT INTO genre (title) VALUES ('Documentary');
+INSERT INTO genre (title) VALUES ('Cartoon');
+INSERT INTO genre (title) VALUES ('SciFi');
+
+INSERT INTO rating (title, description) VALUES ('G', 'General Audiences');
+INSERT INTO rating (title, description) VALUES ('PG', 'Parental Guidance Suggested');
+INSERT INTO rating (title, description) VALUES ('PG-13', 'Parents Strongly Cautioned');
+INSERT INTO rating (title, description) VALUES ('R', 'Restricted');
+INSERT INTO rating (title, description) VALUES ('NC-17', 'Adults Only');
